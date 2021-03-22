258c1994ab1afcad9e5bb89831d2cd9ca87061c3
Paul Goergen <pagkt2@gmail.com>
2021-03-22 00:09:38 -0700

    just can't get this thing to work...

--- core.js
+++ core.js
@@ -1,8 +1,11 @@
 const MOUSE_VISITED_CLASSNAME = 'crx_mouse_over_waves';
 
+// const imgURL = chrome.runtime.getURL("images/surfer-cursor-32px.gif");
+
 document.addEventListener('mousemove', function (e) {
   const srcElement = e.srcElement
   if (srcElement.className === 'jw-video jw-reset' && srcElement.nodeName === 'VIDEO') {
     srcElement.classList.add(MOUSE_VISITED_CLASSNAME);
+    // document.getElementsByClassName(MOUSE_VISITED_CLASSNAME).src = imgURL
   }
 })
