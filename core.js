fda558f158169d845c0e4c8fc092b907b9335b19
Paul Goergen <pagkt2@gmail.com>
2021-03-28 19:58:09 -0700

    We got surfer cursor

--- core.js
+++ core.js
@@ -1,11 +1,14 @@
 const MOUSE_VISITED_CLASSNAME = 'crx_mouse_over_waves';
 
-// const imgURL = chrome.runtime.getURL("images/surfer-cursor-32px.gif");
+const imgURL = chrome.runtime.getURL("images/surfer-cursor-32px.cur");
 
-document.addEventListener('mousemove', function (e) {
-  const srcElement = e.srcElement
-  if (srcElement.className === 'jw-video jw-reset' && srcElement.nodeName === 'VIDEO') {
-    srcElement.classList.add(MOUSE_VISITED_CLASSNAME);
-    // document.getElementsByClassName(MOUSE_VISITED_CLASSNAME).src = imgURL
+document.addEventListener('mousemove', () => {
+  const waves = document.getElementsByClassName('jw-video jw-reset')
+  if (waves.length){
+    for (const wave of waves) {
+      if (!wave.style.cursor) {
+        wave.style.cursor = `url(${imgURL}), progress`
+      }
+    }
   }
 })
